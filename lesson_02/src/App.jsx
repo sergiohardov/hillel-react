40bca9bb6c54e80e027155b06e0a9efe83888c73
Sergio Hardov <sergiohardov@gmail.com>
2024-03-07 15:37:33 +0200

    Lesson 02

--- lesson_02/src/App.jsx
+++ lesson_02/src/App.jsx
@@ -1,5 +1,12 @@
+import List from "./components/List/List";
+import cities from "./data/cities";
+
 function App() {
-  return <p>App</p>;
+  return (
+    <>
+      <List list={cities} color="lightblue" />
+    </>
+  );
 }
 
 export default App;
